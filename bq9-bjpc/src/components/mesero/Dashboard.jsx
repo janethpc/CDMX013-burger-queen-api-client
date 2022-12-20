0df1e5915e045c5029d1249a0af0659986674e6f
Janeth Pérez <janeth_2124@hotmail.com>
2022-12-20 16:02:59 -0600

    firts  modal implemetation

--- bq9-bjpc/src/components/mesero/Dashboard.jsx
+++ bq9-bjpc/src/components/mesero/Dashboard.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Outlet, Link } from 'react-router-dom'
 import { NavBar } from '../../NavBar/NavBar'
+import { Modal } from './modal'
 
 
 
 export const Dashboard = () => {
+
+
     //outlet trabaja la composiciòn
   return (
     <>
@@ -13,6 +16,9 @@ export const Dashboard = () => {
           <div className='text-center'>
          <h1>welcome</h1>
          <Link to={'/NewOrder'}><button>Nueva Orden</button></Link>
+         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        modal </button>
+        <modal/>
          <Link to={'/AllOrder'}><button>Historial De Pedidos</button></Link>
          </div>
          <div className='row'>
